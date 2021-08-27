fe5cd59d9a983b464fcf91074344d7caba9620db
JoshLaunchcode <holo.sf0@gmail.com>
2021-08-26 19:49:48 -0500

    Finished Studio 1!

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,29 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args){
+        double radius;
+        boolean verified = false;
+
+        while(!verified) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is the radius of your circle?");
+            if(input.hasNextDouble()) {
+                radius = input.nextDouble();
+                if(radius >= 0){
+                    verified = true;
+                    System.out.println("Area is " + Circle.getArea(radius));
+                } else {
+                    Error.msg();
+                }
+            } else {
+                Error.msg();
+            }
+
+        }
+
+    }
+
+}
---
+++ src/org/launchcode/java/studios/areaofacircle/Error.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Error {
+    public static void msg(){
+        System.out.println("Please enter a positive numerical value.");
+    }
+}
